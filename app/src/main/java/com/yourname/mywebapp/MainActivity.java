4d0eb8d6f45a7e88eedf2626c37adb0f8297c78a
malikfaizangamerr-coder <malikfaizangamerr@gmail.com>
2026-09-02 12:40:28 +0500

    Add MainActivity with WebView implementation

---
+++ app/src/main/java/com/yourname/mywebapp/MainActivity.java
@@ -0,0 +1,42 @@
+package com.yourname.mywebapp;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Initialize WebView
+        webView = findViewById(R.id.webView);
+
+        // Enable JavaScript
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.getSettings().setLoadWithOverviewMode(true);
+        webView.getSettings().setUseWideViewPort(true);
+
+        // Open links inside app (not browser)
+        webView.setWebViewClient(new WebViewClient());
+
+        // *** CHANGE THIS URL TO YOUR WEBSITE ***
+        webView.loadUrl("https://your-website.com");
+    }
+
+    // Back button navigation
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
